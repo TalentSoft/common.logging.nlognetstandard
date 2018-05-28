993800f8bc046832d4b7d7255ca006240bd86314
Jean-Bernard Lagorce <jblagorce@talentsoft.com>
2018-05-28 10:28:25 +0200

    Added a dummy test for this external library

---
+++ Common.Logging.NLogNetStandard.Nunit/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Logging.NLogNetStandard.Nunit
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void TestThatPasses()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
